463ad8326b2f75b5806a2238c5a9c51dbe62d723
conor <doyle.conorp@gmail.com>
2022-07-02 17:06:26 -0700

    Add correct func behavior

--- scripts.js
+++ scripts.js
@@ -1,21 +1,29 @@
 // Add, subtract, multiply, divide functions
 function add(...nums) {
-    const sum = nums.reduce((ttl, num) => ttl + num);
+    const sum = nums.reduce(
+        (ttl, num) => ttl + num
+        );
     return sum;
 }
 
 function subtract(...nums) {
-    const difference = nums.reduce((ttl, num) => ttl - num);
+    const difference = nums.reduce(
+        (ttl, num) => ttl - num
+        );
     return difference;
 }
 
 function multiply(...nums) {
-    const product = nums.reduce((ttl, num) => ttl * num);
+    const product = nums.reduce(
+        (ttl, num) => ttl * num
+        );
     return product;
 }
 
 function divide (...nums) {
-    const quotient = nums.reduce((ttl, num) => ttl / num);
+    const quotient = nums.reduce(
+        (ttl, num) => ttl / num
+        );
     return quotient;
 }
 
@@ -23,8 +31,8 @@ function divide (...nums) {
 // Calculator operator function
 function operate(a, b, operator) {
     switch (operator) {
-        case '+':
-            return add(a, b);
+        case '+': // operate(2, 3, '+')
+            return add(a, b); // 5
         case '-':
             return subtract(a, b);
         case '*':
